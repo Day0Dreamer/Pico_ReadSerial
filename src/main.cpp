ca7ee5794c210c300cb6fd8fc9f6a6c3c5011c77
DayDreamer <poisonsonic@gmail.com>
2022-01-10 15:37:14 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+
+#include "pico/stdlib.h"
+#include "pico/stdio_uart.h"
+
+void setup();
+void loop();
+
+void setup_arduino_way(){
+  // Init Serial
+  Serial1.setTX(12);
+  Serial1.setRX(13);
+  // Start serial
+  Serial1.begin(115200);
+}
+String buffer_arduino;
+void read_arduino_way(){
+  if (Serial1.available()) {
+    buffer_arduino = Serial1.readString();
+    Serial1.print(buffer_arduino);
+    if (buffer_arduino == "0") {
+    }
+    if (buffer_arduino == "\r" || buffer_arduino == "\n") {
+      Serial1.print("\n\r");
+    }
+  }
+}
+
+char buffer_pico;
+void setup_pico_way(){
+  stdio_uart_init_full(uart0, 115200, 12, 13);
+}
+void read_pico_way(){
+  while (true) {
+    if (uart_is_readable(uart0)) {
+      buffer_pico = getchar();
+      printf("%c", buffer_pico);
+      if (buffer_pico == '\r' || buffer_pico == '\n') {
+        printf("\n\r");
+      }
+    }
+    printf("\r\n");
+    sleep_ms(10);
+  }
+}
+
+void setup() {
+//  setup_arduino_way();
+  setup_pico_way();
+}
+
+void loop() {
+//  read_arduino_way();
+  read_pico_way();
+}
